e90917d647f79bc8dbd580e54d31aaed0cc439ed
titlethanason <titlethanason@gmail.com>
2020-01-27 11:40:38 +0700

    init

---
+++ frcnn.py
@@ -0,0 +1,255 @@
+from keras import backend as K
+from tensorflow.python.keras.backend import set_session
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Flatten, Dense, Input, Conv2D, MaxPooling2D, Dropout
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, TimeDistributed
+from keras.engine.topology import get_source_inputs
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.objectives import categorical_crossentropy
+
+from keras.models import Model
+from keras.utils import generic_utils
+from keras.engine import Layer, InputSpec
+from keras import initializers, regularizers
+import numpy as np
+import copy
+import random
+import cv2
+import base64
+import math
+import pickle
+import sys
+
+from flask import Flask, jsonify, request
+from frcnn_layers import *
+from rpn_network import *
+
+class Config:
+	def __init__(self):
+		self.verbose = True
+		self.network = 'resnet'
+		self.use_horizontal_flips = False
+		self.use_vertical_flips = False
+		self.rot_90 = False
+		self.anchor_box_scales = [64, 128, 256] 
+		self.anchor_box_ratios = [[1, 1], [1./math.sqrt(2), 2./math.sqrt(2)], [2./math.sqrt(2), 1./math.sqrt(2)]]
+		self.im_size = 600
+		self.img_channel_mean = [103.939, 116.779, 123.68]
+		self.img_scaling_factor = 1.0
+		self.num_rois = 4
+		self.rpn_stride = 16
+
+		self.balanced_classes = False
+		self.std_scaling = 4.0
+		self.classifier_regr_std = [8.0, 8.0, 4.0, 4.0]
+
+		self.rpn_min_overlap = 0.3
+		self.rpn_max_overlap = 0.7
+
+		self.classifier_min_overlap = 0.1
+		self.classifier_max_overlap = 0.5
+
+		self.class_mapping = None
+		self.model_path = None
+
+
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+global sess
+global graph
+sess = tf.Session(config=config)
+set_session(sess)
+graph = tf.get_default_graph()
+sys.setrecursionlimit(40000)
+
+from frcnn_layers import *
+from rpn_network import *
+
+def format_img_size(img, C):
+	img_min_side = float(C.im_size)
+	(height,width,_) = img.shape
+		
+	if width <= height:
+		ratio = img_min_side/width
+		new_height = int(ratio * height)
+		new_width = int(img_min_side)
+	else:
+		ratio = img_min_side/height
+		new_width = int(ratio * width)
+		new_height = int(img_min_side)
+	img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+	return img, ratio	
+
+def format_img_channels(img, C):
+	img = img[:, :, (2, 1, 0)]
+	img = img.astype(np.float32)
+	img[:, :, 0] -= C.img_channel_mean[0]
+	img[:, :, 1] -= C.img_channel_mean[1]
+	img[:, :, 2] -= C.img_channel_mean[2]
+	img /= C.img_scaling_factor
+	img = np.transpose(img, (2, 0, 1))
+	img = np.expand_dims(img, axis=0)
+	return img
+
+def format_img(img, C):
+	img, ratio = format_img_size(img, C)
+	img = format_img_channels(img, C)
+	return img, ratio
+
+def get_real_coordinates(ratio, x1, y1, x2, y2):
+
+	real_x1 = int(round(x1 // ratio))
+	real_y1 = int(round(y1 // ratio))
+	real_x2 = int(round(x2 // ratio))
+	real_y2 = int(round(y2 // ratio))
+
+	return (real_x1, real_y1, real_x2 ,real_y2)
+
+def get_predict(img):
+    bbox_threshold = 0.7
+    X, ratio = format_img(img, C)
+    st = time.time()
+    X = np.transpose(X, (0, 2, 3, 1))
+    global sess
+    set_session(sess)
+    global graph
+    with graph.as_default():
+        [Y1, Y2, F] = model_rpn.predict(X)
+    R = rpn_to_roi(Y1, Y2, C, 'tf', overlap_thresh=0.7)
+    R[:, 2] -= R[:, 0]
+    R[:, 3] -= R[:, 1]
+
+    bboxes = {}
+    probs = {}
+
+    for jk in range(R.shape[0]//C.num_rois + 1):
+        ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
+        if ROIs.shape[1] == 0:
+            break
+
+        if jk == R.shape[0]//C.num_rois:
+            #pad R
+            curr_shape = ROIs.shape
+            target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
+            ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+            ROIs_padded[:, :curr_shape[1], :] = ROIs
+            ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
+            ROIs = ROIs_padded
+        with graph.as_default():
+            [P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
+
+        # Calculate bboxes coordinates on resized image
+        for ii in range(P_cls.shape[1]):
+            # Ignore 'bg' class
+            if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
+                continue
+
+            cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
+
+            if cls_name not in bboxes:
+                bboxes[cls_name] = []
+                probs[cls_name] = []
+
+            (x, y, w, h) = ROIs[0, ii, :]
+
+            cls_num = np.argmax(P_cls[0, ii, :])
+            try:
+                (tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+                tx /= C.classifier_regr_std[0]
+                ty /= C.classifier_regr_std[1]
+                tw /= C.classifier_regr_std[2]
+                th /= C.classifier_regr_std[3]
+                x, y, w, h = apply_regr(x, y, w, h, tx, ty, tw, th)
+            except:
+                pass
+            bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
+            probs[cls_name].append(np.max(P_cls[0, ii, :]))
+
+    all_dets = []
+
+    for key in bboxes:
+        bbox = np.array(bboxes[key])
+
+        new_boxes, new_probs = non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.2)
+        for jk in range(new_boxes.shape[0]):
+            (x1, y1, x2, y2) = new_boxes[jk,:]
+            (real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
+
+            #cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),4)
+
+            #textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
+            all_dets.append((key,100*new_probs[jk],real_x1,real_x2,real_y1,real_y2))
+
+            # (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
+            # textOrg = (real_x1, real_y1-0)
+
+            # cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 1)
+            # cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
+            # cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+
+    #print('Elapsed time = {}'.format(time.time() - st))
+    return all_dets
+    #cv2.imwrite('./results_imgs/test.png')
+
+def readb64(encoded_data):
+   nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+   return img
+
+model_best_path = './model/model_frcnn_resnet_best.hdf5'
+C = pickle.load( open("./model_resnet_config.pickle", "rb" ))
+C.use_horizontal_flips = False
+C.use_vertical_flips = False
+C.rot_90 = False
+
+class_mapping = C.class_mapping
+class_mapping = {v: k for k, v in class_mapping.items()}
+class_to_color = {class_mapping[v]: np.random.randint(0, 255, 3) for v in class_mapping}
+
+num_features = 1024
+input_shape_img = (None, None, 3)
+input_shape_features = (None, None, num_features)
+img_input = Input(shape=input_shape_img)
+roi_input = Input(shape=(C.num_rois, 4))
+feature_map_input = Input(shape=input_shape_features)
+shared_layers = nn_base(img_input, trainable=True)
+num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+rpn_layers = rpn_layer(shared_layers, num_anchors)
+classifier = classifier_layer(feature_map_input, roi_input, C.num_rois, nb_classes=len(C.class_mapping))
+model_rpn = Model(img_input, rpn_layers)
+model_classifier_only = Model([feature_map_input, roi_input], classifier)
+model_classifier = Model([feature_map_input, roi_input], classifier)
+
+print('Loading weights from {}'.format(model_best_path))
+model_rpn.load_weights(model_best_path, by_name=True)
+model_classifier.load_weights(model_best_path, by_name=True)
+
+model_rpn.compile(optimizer='sgd', loss='mse')
+model_classifier.compile(optimizer='sgd', loss='mse')
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    start_time = time.time()
+    ##recieve image
+    data = request.get_json(force=True)
+    image = data['photo']
+    decoded_img = readb64(image)
+
+    ##display shape and save (debug)
+    #print(decoded_img.shape)
+    #cv2.imwrite('./test.png',decoded_img)
+
+    ##predict and return list of results
+    results = get_predict(decoded_img)
+    #print(results)
+
+    ##response back in json format 
+    print('PROCESS Elapsed time = {}'.format(time.time() - start_time))
+    return jsonify({"results": results}), 200
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=False)
---
+++ rpn_network.py
@@ -0,0 +1,464 @@
+import numpy as np
+import copy
+import random
+import cv2
+import math
+
+def union(au, bu, area_intersection):
+	area_a = (au[2] - au[0]) * (au[3] - au[1])
+	area_b = (bu[2] - bu[0]) * (bu[3] - bu[1])
+	area_union = area_a + area_b - area_intersection
+	return area_union
+
+def intersection(ai, bi):
+	x = max(ai[0], bi[0])
+	y = max(ai[1], bi[1])
+	w = min(ai[2], bi[2]) - x
+	h = min(ai[3], bi[3]) - y
+	if w < 0 or h < 0:
+		return 0
+	return w*h
+
+def iou(a, b):
+	if a[0] >= a[2] or a[1] >= a[3] or b[0] >= b[2] or b[1] >= b[3]:
+		return 0.0
+
+	area_i = intersection(a, b)
+	area_u = union(a, b, area_i)
+
+	return float(area_i) / float(area_u + 1e-6)
+
+def calc_rpn(C, img_data, width, height, resized_width, resized_height, img_length_calc_function):
+	downscale = float(C.rpn_stride) 
+	anchor_sizes = C.anchor_box_scales   
+	anchor_ratios = C.anchor_box_ratios  
+	num_anchors = len(anchor_sizes) * len(anchor_ratios) 
+
+	(output_width, output_height) = img_length_calc_function(resized_width, resized_height)
+
+	n_anchratios = len(anchor_ratios)
+	
+	y_rpn_overlap = np.zeros((output_height, output_width, num_anchors))
+	y_is_box_valid = np.zeros((output_height, output_width, num_anchors))
+	y_rpn_regr = np.zeros((output_height, output_width, num_anchors * 4))
+
+	num_bboxes = len(img_data['bboxes'])
+
+	num_anchors_for_bbox = np.zeros(num_bboxes).astype(int)
+	best_anchor_for_bbox = -1*np.ones((num_bboxes, 4)).astype(int)
+	best_iou_for_bbox = np.zeros(num_bboxes).astype(np.float32)
+	best_x_for_bbox = np.zeros((num_bboxes, 4)).astype(int)
+	best_dx_for_bbox = np.zeros((num_bboxes, 4)).astype(np.float32)
+
+	gta = np.zeros((num_bboxes, 4))
+	for bbox_num, bbox in enumerate(img_data['bboxes']):
+		gta[bbox_num, 0] = bbox['x1'] * (resized_width / float(width))
+		gta[bbox_num, 1] = bbox['x2'] * (resized_width / float(width))
+		gta[bbox_num, 2] = bbox['y1'] * (resized_height / float(height))
+		gta[bbox_num, 3] = bbox['y2'] * (resized_height / float(height))
+
+	for anchor_size_idx in range(len(anchor_sizes)):
+		for anchor_ratio_idx in range(n_anchratios):
+			anchor_x = anchor_sizes[anchor_size_idx] * anchor_ratios[anchor_ratio_idx][0]
+			anchor_y = anchor_sizes[anchor_size_idx] * anchor_ratios[anchor_ratio_idx][1]	
+			
+			for ix in range(output_width):						
+				x1_anc = downscale * (ix + 0.5) - anchor_x / 2
+				x2_anc = downscale * (ix + 0.5) + anchor_x / 2	
+								
+				if x1_anc < 0 or x2_anc > resized_width:
+					continue
+					
+				for jy in range(output_height):
+
+					y1_anc = downscale * (jy + 0.5) - anchor_y / 2
+					y2_anc = downscale * (jy + 0.5) + anchor_y / 2
+
+					if y1_anc < 0 or y2_anc > resized_height:
+						continue
+
+					bbox_type = 'neg'
+					best_iou_for_loc = 0.0
+
+					for bbox_num in range(num_bboxes):
+						curr_iou = iou([gta[bbox_num, 0], gta[bbox_num, 2], gta[bbox_num, 1], gta[bbox_num, 3]], [x1_anc, y1_anc, x2_anc, y2_anc])
+						if curr_iou > best_iou_for_bbox[bbox_num] or curr_iou > C.rpn_max_overlap:
+							cx = (gta[bbox_num, 0] + gta[bbox_num, 1]) / 2.0
+							cy = (gta[bbox_num, 2] + gta[bbox_num, 3]) / 2.0
+							cxa = (x1_anc + x2_anc)/2.0
+							cya = (y1_anc + y2_anc)/2.0
+
+							tx = (cx - cxa) / (x2_anc - x1_anc)
+							ty = (cy - cya) / (y2_anc - y1_anc)
+							tw = np.log((gta[bbox_num, 1] - gta[bbox_num, 0]) / (x2_anc - x1_anc))
+							th = np.log((gta[bbox_num, 3] - gta[bbox_num, 2]) / (y2_anc - y1_anc))
+						
+						if img_data['bboxes'][bbox_num]['class'] != 'bg':
+							if curr_iou > best_iou_for_bbox[bbox_num]:
+								best_anchor_for_bbox[bbox_num] = [jy, ix, anchor_ratio_idx, anchor_size_idx]
+								best_iou_for_bbox[bbox_num] = curr_iou
+								best_x_for_bbox[bbox_num,:] = [x1_anc, x2_anc, y1_anc, y2_anc]
+								best_dx_for_bbox[bbox_num,:] = [tx, ty, tw, th]
+
+							if curr_iou > C.rpn_max_overlap:
+								bbox_type = 'pos'
+								num_anchors_for_bbox[bbox_num] += 1
+								if curr_iou > best_iou_for_loc:
+									best_iou_for_loc = curr_iou
+									best_regr = (tx, ty, tw, th)
+
+							if C.rpn_min_overlap < curr_iou < C.rpn_max_overlap:
+								if bbox_type != 'pos':
+									bbox_type = 'neutral'
+
+					if bbox_type == 'neg':
+						y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+						y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
+					elif bbox_type == 'neutral':
+						y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
+						y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
+					elif bbox_type == 'pos':
+						y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+						y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+						start = 4 * (anchor_ratio_idx + n_anchratios * anchor_size_idx)
+						y_rpn_regr[jy, ix, start:start+4] = best_regr
+
+	for idx in range(num_anchors_for_bbox.shape[0]):
+		if num_anchors_for_bbox[idx] == 0:
+			if best_anchor_for_bbox[idx, 0] == -1:
+				continue
+			y_is_box_valid[
+				best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], best_anchor_for_bbox[idx,2] + n_anchratios *
+				best_anchor_for_bbox[idx,3]] = 1
+			y_rpn_overlap[
+				best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], best_anchor_for_bbox[idx,2] + n_anchratios *
+				best_anchor_for_bbox[idx,3]] = 1
+			start = 4 * (best_anchor_for_bbox[idx,2] + n_anchratios * best_anchor_for_bbox[idx,3])
+			y_rpn_regr[
+				best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], start:start+4] = best_dx_for_bbox[idx, :]
+
+	y_rpn_overlap = np.transpose(y_rpn_overlap, (2, 0, 1))
+	y_rpn_overlap = np.expand_dims(y_rpn_overlap, axis=0)
+
+	y_is_box_valid = np.transpose(y_is_box_valid, (2, 0, 1))
+	y_is_box_valid = np.expand_dims(y_is_box_valid, axis=0)
+
+	y_rpn_regr = np.transpose(y_rpn_regr, (2, 0, 1))
+	y_rpn_regr = np.expand_dims(y_rpn_regr, axis=0)
+
+	pos_locs = np.where(np.logical_and(y_rpn_overlap[0, :, :, :] == 1, y_is_box_valid[0, :, :, :] == 1))
+	neg_locs = np.where(np.logical_and(y_rpn_overlap[0, :, :, :] == 0, y_is_box_valid[0, :, :, :] == 1))
+
+	num_pos = len(pos_locs[0])
+	num_regions = 256
+
+	if len(pos_locs[0]) > num_regions/2:
+		val_locs = random.sample(range(len(pos_locs[0])), len(pos_locs[0]) - num_regions/2)
+		y_is_box_valid[0, pos_locs[0][val_locs], pos_locs[1][val_locs], pos_locs[2][val_locs]] = 0
+		num_pos = num_regions/2
+
+	if len(neg_locs[0]) + num_pos > num_regions:
+		val_locs = random.sample(range(len(neg_locs[0])), len(neg_locs[0]) - num_pos)
+		y_is_box_valid[0, neg_locs[0][val_locs], neg_locs[1][val_locs], neg_locs[2][val_locs]] = 0
+
+	y_rpn_cls = np.concatenate([y_is_box_valid, y_rpn_overlap], axis=1)
+	y_rpn_regr = np.concatenate([np.repeat(y_rpn_overlap, 4, axis=1), y_rpn_regr], axis=1)
+
+	return np.copy(y_rpn_cls), np.copy(y_rpn_regr), num_pos
+def get_new_img_size(width, height, img_min_side=300):
+	if width <= height:
+		f = float(img_min_side) / width
+		resized_height = int(f * height)
+		resized_width = img_min_side
+	else:
+		f = float(img_min_side) / height
+		resized_width = int(f * width)
+		resized_height = img_min_side
+
+	return resized_width, resized_height
+
+def augment(img_data, config, augment=True):
+	assert 'filepath' in img_data
+	assert 'bboxes' in img_data
+	assert 'width' in img_data
+	assert 'height' in img_data
+
+	img_data_aug = copy.deepcopy(img_data)
+
+	img = cv2.imread(img_data_aug['filepath'])
+
+	if augment:
+		rows, cols = img.shape[:2]
+
+		if config.use_horizontal_flips and np.random.randint(0, 2) == 0:
+			img = cv2.flip(img, 1)
+			for bbox in img_data_aug['bboxes']:
+				x1 = bbox['x1']
+				x2 = bbox['x2']
+				bbox['x2'] = cols - x1
+				bbox['x1'] = cols - x2
+
+		if config.use_vertical_flips and np.random.randint(0, 2) == 0:
+			img = cv2.flip(img, 0)
+			for bbox in img_data_aug['bboxes']:
+				y1 = bbox['y1']
+				y2 = bbox['y2']
+				bbox['y2'] = rows - y1
+				bbox['y1'] = rows - y2
+
+		if config.rot_90:
+			angle = np.random.choice([0,90,180,270],1)[0]
+			if angle == 270:
+				img = np.transpose(img, (1,0,2))
+				img = cv2.flip(img, 0)
+			elif angle == 180:
+				img = cv2.flip(img, -1)
+			elif angle == 90:
+				img = np.transpose(img, (1,0,2))
+				img = cv2.flip(img, 1)
+			elif angle == 0:
+				pass
+
+			for bbox in img_data_aug['bboxes']:
+				x1 = bbox['x1']
+				x2 = bbox['x2']
+				y1 = bbox['y1']
+				y2 = bbox['y2']
+				if angle == 270:
+					bbox['x1'] = y1
+					bbox['x2'] = y2
+					bbox['y1'] = cols - x2
+					bbox['y2'] = cols - x1
+				elif angle == 180:
+					bbox['x2'] = cols - x1
+					bbox['x1'] = cols - x2
+					bbox['y2'] = rows - y1
+					bbox['y1'] = rows - y2
+				elif angle == 90:
+					bbox['x1'] = rows - y2
+					bbox['x2'] = rows - y1
+					bbox['y1'] = x1
+					bbox['y2'] = x2        
+				elif angle == 0:
+					pass
+
+	img_data_aug['width'] = img.shape[1]
+	img_data_aug['height'] = img.shape[0]
+	return img_data_aug, img
+
+def get_anchor_gt(all_img_data, C, img_length_calc_function, mode='train'):
+	while True:
+
+		for img_data in all_img_data:
+			try:
+
+				if mode == 'train':
+					img_data_aug, x_img = augment(img_data, C, augment=True)
+				else:
+					img_data_aug, x_img = augment(img_data, C, augment=False)
+
+				(width, height) = (img_data_aug['width'], img_data_aug['height'])
+				(rows, cols, _) = x_img.shape
+
+				assert cols == width
+				assert rows == height
+
+				(resized_width, resized_height) = get_new_img_size(width, height, C.im_size)
+
+				x_img = cv2.resize(x_img, (resized_width, resized_height), interpolation=cv2.INTER_CUBIC)
+				debug_img = x_img.copy()
+
+				try:
+					y_rpn_cls, y_rpn_regr, num_pos = calc_rpn(C, img_data_aug, width, height, resized_width, resized_height, img_length_calc_function)
+				except:
+					continue
+
+				x_img = x_img[:,:, (2, 1, 0)]  # BGR -> RGB
+				x_img = x_img.astype(np.float32)
+				x_img[:, :, 0] -= C.img_channel_mean[0]
+				x_img[:, :, 1] -= C.img_channel_mean[1]
+				x_img[:, :, 2] -= C.img_channel_mean[2]
+				x_img /= C.img_scaling_factor
+
+				x_img = np.transpose(x_img, (2, 0, 1))
+				x_img = np.expand_dims(x_img, axis=0)
+
+				y_rpn_regr[:, y_rpn_regr.shape[1]//2:, :, :] *= C.std_scaling
+
+				x_img = np.transpose(x_img, (0, 2, 3, 1))
+				y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1))
+				y_rpn_regr = np.transpose(y_rpn_regr, (0, 2, 3, 1))
+
+				yield np.copy(x_img), [np.copy(y_rpn_cls), np.copy(y_rpn_regr)], img_data_aug, debug_img, num_pos
+
+			except Exception as e:
+				print(e)
+				continue
+
+def non_max_suppression_fast(boxes, probs, overlap_thresh=0.9, max_boxes=300):
+	if len(boxes) == 0:
+		return []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	np.testing.assert_array_less(x1, x2)
+	np.testing.assert_array_less(y1, y2)
+
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	pick = []
+
+	area = (x2 - x1) * (y2 - y1)
+
+	idxs = np.argsort(probs)
+
+	while len(idxs) > 0:
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		xx1_int = np.maximum(x1[i], x1[idxs[:last]])
+		yy1_int = np.maximum(y1[i], y1[idxs[:last]])
+		xx2_int = np.minimum(x2[i], x2[idxs[:last]])
+		yy2_int = np.minimum(y2[i], y2[idxs[:last]])
+
+		ww_int = np.maximum(0, xx2_int - xx1_int)
+		hh_int = np.maximum(0, yy2_int - yy1_int)
+
+		area_int = ww_int * hh_int
+
+		area_union = area[i] + area[idxs[:last]] - area_int
+		overlap = area_int/(area_union + 1e-6)
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlap_thresh)[0])))
+
+		if len(pick) >= max_boxes:
+			break
+
+	boxes = boxes[pick].astype("int")
+	probs = probs[pick]
+	return boxes, probs
+
+def apply_regr_np(X, T):
+	try:
+		x = X[0, :, :]
+		y = X[1, :, :]
+		w = X[2, :, :]
+		h = X[3, :, :]
+
+		tx = T[0, :, :]
+		ty = T[1, :, :]
+		tw = T[2, :, :]
+		th = T[3, :, :]
+
+		cx = x + w/2.
+		cy = y + h/2.
+		cx1 = tx * w + cx
+		cy1 = ty * h + cy
+
+		w1 = np.exp(tw.astype(np.float64)) * w
+		h1 = np.exp(th.astype(np.float64)) * h
+		x1 = cx1 - w1/2.
+		y1 = cy1 - h1/2.
+
+		x1 = np.round(x1)
+		y1 = np.round(y1)
+		w1 = np.round(w1)
+		h1 = np.round(h1)
+		return np.stack([x1, y1, w1, h1])
+	except Exception as e:
+		print(e)
+		return X
+    
+def apply_regr(x, y, w, h, tx, ty, tw, th):
+	try:
+		cx = x + w/2.
+		cy = y + h/2.
+		cx1 = tx * w + cx
+		cy1 = ty * h + cy
+		w1 = math.exp(tw) * w
+		h1 = math.exp(th) * h
+		x1 = cx1 - w1/2.
+		y1 = cy1 - h1/2.
+		x1 = int(round(x1))
+		y1 = int(round(y1))
+		w1 = int(round(w1))
+		h1 = int(round(h1))
+
+		return x1, y1, w1, h1
+
+	except ValueError:
+		return x, y, w, h
+	except OverflowError:
+		return x, y, w, h
+	except Exception as e:
+		print(e)
+		return x, y, w, h   
+
+def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=300,overlap_thresh=0.9):
+	regr_layer = regr_layer / C.std_scaling
+
+	anchor_sizes = C.anchor_box_scales   # (3 in here)
+	anchor_ratios = C.anchor_box_ratios  # (3 in here)
+
+	assert rpn_layer.shape[0] == 1
+
+	(rows, cols) = rpn_layer.shape[1:3]
+
+	curr_layer = 0
+	A = np.zeros((4, rpn_layer.shape[1], rpn_layer.shape[2], rpn_layer.shape[3]))
+
+	for anchor_size in anchor_sizes:
+		for anchor_ratio in anchor_ratios:
+			anchor_x = (anchor_size * anchor_ratio[0])/C.rpn_stride
+			anchor_y = (anchor_size * anchor_ratio[1])/C.rpn_stride
+			
+			regr = regr_layer[0, :, :, 4 * curr_layer:4 * curr_layer + 4] # shape => (18, 25, 4)
+			regr = np.transpose(regr, (2, 0, 1)) # shape => (4, 18, 25)
+
+			X, Y = np.meshgrid(np.arange(cols),np. arange(rows))
+
+			A[0, :, :, curr_layer] = X - anchor_x/2 # Top left x coordinate
+			A[1, :, :, curr_layer] = Y - anchor_y/2 # Top left y coordinate
+			A[2, :, :, curr_layer] = anchor_x       # width of current anchor
+			A[3, :, :, curr_layer] = anchor_y       # height of current anchor
+
+			if use_regr:
+				A[:, :, :, curr_layer] = apply_regr_np(A[:, :, :, curr_layer], regr)
+
+			A[2, :, :, curr_layer] = np.maximum(1, A[2, :, :, curr_layer])
+			A[3, :, :, curr_layer] = np.maximum(1, A[3, :, :, curr_layer])
+
+			A[2, :, :, curr_layer] += A[0, :, :, curr_layer]
+			A[3, :, :, curr_layer] += A[1, :, :, curr_layer]
+
+			A[0, :, :, curr_layer] = np.maximum(0, A[0, :, :, curr_layer])
+			A[1, :, :, curr_layer] = np.maximum(0, A[1, :, :, curr_layer])
+			A[2, :, :, curr_layer] = np.minimum(cols-1, A[2, :, :, curr_layer])
+			A[3, :, :, curr_layer] = np.minimum(rows-1, A[3, :, :, curr_layer])
+
+			curr_layer += 1
+
+	all_boxes = np.reshape(A.transpose((0, 3, 1, 2)), (4, -1)).transpose((1, 0))  # shape=(4050, 4)
+	all_probs = rpn_layer.transpose((0, 3, 1, 2)).reshape((-1))                   # shape=(4050,)
+
+	x1 = all_boxes[:, 0]
+	y1 = all_boxes[:, 1]
+	x2 = all_boxes[:, 2]
+	y2 = all_boxes[:, 3]
+
+	idxs = np.where((x1 - x2 >= 0) | (y1 - y2 >= 0))
+
+	all_boxes = np.delete(all_boxes, idxs, 0)
+	all_probs = np.delete(all_probs, idxs, 0)
+
+	result = non_max_suppression_fast(all_boxes, all_probs, overlap_thresh=overlap_thresh, max_boxes=max_boxes)[0]
+
+	return result  
+
+print("Done read rpn_network")
